df9bbba70c1ea6068d6544b70c8d8b758f902839
TheBCode <bbuyck1@gmail.com>
2016-11-08 19:20:57 +0000

    Added edit/index views to controller

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,10 +1,24 @@
 class ArticlesController < ApplicationController
   def index
-    
+    @articles = Article.all
   end
   
   def new
     @article = Article.new
+  end
+  
+  def edit
+    @article = Article.find(params[:id])
+  end
+  
+  def update
+    @article = Article.find(params[:id])
+    if @article.update(article_params)
+      flash[:notice] = "Article was successfully updated"
+      redirect_to article_path(@article)
+    else
+      render 'edit'
+    end
   end
   
   def create
@@ -20,6 +34,8 @@ class ArticlesController < ApplicationController
   def show
     @article = Article.find(params[:id])
   end
+  
+  
   
   private 
   def article_params
